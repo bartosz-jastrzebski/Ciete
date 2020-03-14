e8f95bb7e24b56b4566601196e8f86d9a0fe0369
bartosz-jastrzebski <47112179+bartosz-jastrzebski@users.noreply.github.com>
2020-03-14 17:42:11 +0100

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,16 +5,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = "ciete.settings"
 
 
 INTERP = "/home/ciete/.virtualenvs/ciete-django/bin/python3.6"
-#INTERP is present twice so that the new python interpreter 
-#knows the actual executable path 
+
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
-
-# cwd = os.getcwd()
-# sys.path.append(cwd)
-# sys.path.append(cwd + '/ciete')  #You must add your project here
-
-# sys.path.insert(0,cwd+'/venv/bin')
-# sys.path.insert(0,cwd+'/venv/lib/python3.6/site-packages')
 
 import ciete.wsgi
 
