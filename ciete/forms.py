a0295d4c19ca9847629f045044b22bb39a298dc9
bartosz-jastrzebski <bartosz.jastrzebski89@gmail.com>
2020-05-14 12:53:25 +0200

    Changed default language and some css

--- ciete/forms.py
+++ ciete/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=60,
-                           widget=forms.TextInput(attrs={'placeholder': _('Imię:')}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': _('Email:')}))
+                           widget=forms.TextInput(attrs={'placeholder': _('name')}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': _('email')}))
     message = forms.CharField(required=True,
-                              widget=forms.Textarea(attrs={'placeholder': _("Wiadmość:")}))
+                              widget=forms.Textarea(attrs={'placeholder': _('message')}))
