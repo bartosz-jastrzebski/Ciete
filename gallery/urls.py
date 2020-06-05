afed8b0552a8eb1d8176621224b28e89a5b90a8a
bartosz-jastrzebski <bartosz.jastrzebski89@gmail.com>
2020-06-05 14:49:16 +0200

    Updated css for ms edge

---
+++ gallery/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf.urls.i18n import i18n_patterns
+
+
+
+url_patterns = i18n_patterns(
+    path('all/',  ,name='all_galleries')
+)
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Gallery
 
-# Create your views here.
+
+def galleries_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 4)
+    page = request.GET.get('page')
+    try: 
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+
+
+
